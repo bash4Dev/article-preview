450033b9ff64601e6bb088dd210f274efe76c210
Bashir Umar Idris <bashtrust15@gmail.com>
2024-06-17 23:52:25 +0100

    Add functionality to the page

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const hover_state = document.querySelector("#hover-state");
+const hover_state = document.querySelector("#hoverState");
 const shareIcon = document.querySelector(".icon-share");
 
-hover_state.style.display = "non";
+hover_state.style.visibility = "hidden";
 
-function onHoverState() {
-    shareIcon.style.backgroundColor = "green";
+function _hover() {
+    const svg = document.getElementById("svgImage");
+    const svgPath = svg.querySelector("path");
+
+    shareIcon.addEventListener("mouseover",  () => {
+        shareIcon.style.backgroundColor = "#6e8098";
+        svgPath.setAttribute("fill", "hsl(0, 0%, 100%)");
+        hover_state.style.visibility = "visible";
+    });
+    
+    shareIcon.addEventListener("mouseout", () => {
+        shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
+        svgPath.setAttribute("fill", "#6E8098");
+        hover_state.style.visibility = "hidden";
+    });
 }
+
+_hover();
